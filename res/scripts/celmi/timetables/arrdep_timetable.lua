4880050731e4b8bf782b9a9ccdec5c4e8bf7beb2
Maja Trela <maja.ada0@gmail.com>
2022-12-17 13:49:04 +0100

    Fixing ArrDepTimetable issues

--- res/scripts/celmi/timetables/arrdep_timetable.lua
+++ res/scripts/celmi/timetables/arrdep_timetable.lua
@@ -14,14 +14,22 @@ function ArrDepTimetable:new(period)
     return setmetatable({period = period, arrdeps = {}}, self)
 end
 
+function ArrDepTimetable:assertInBounds(ts)
+    for _,t in pairs(ts) do
+        assert(0 <= t and t < self.period)
+    end
+end
+
 function ArrDepTimetable:addArrDep(arr, dep)
     assert(dep)
+    self:assertInBounds({arr, dep})
     table.insert(self.arrdeps, {arr = arr, dep = dep})
 end 
 
 function ArrDepTimetable:updateArrDep(ind, newArr, newDep)
     assert(ind and newDep)
     assert(self.arrdeps[ind])
+    self:assertInBounds({newArr, newDep})
     self.arrdeps[ind] = {arr = newArr, dep = newDep}
 end 
 
@@ -31,20 +39,50 @@ function ArrDepTimetable:removeArrDep(ind)
     self.arrdeps[ind] = nil
 end 
 
-function ArrDepTimetable:absTimeDifference(t1, t2)
-    local absDiff = math.abs(t1 - t2)
-    return math.min(absDiff, self.period - absDiff)
+function ArrDepTimetable:timeDiff(t1, t2)
+    self:assertInBounds({t1, t2})
+    return (t1 + self.period - t2) % self.period
 end
+
+function ArrDepTimetable:inInterval(t, l, r)
+    assert(t and l and r) 
+    self:assertInBounds({t, l, r})
+
+    local tonumber = function(value)
+        return value and 1 or 0
+    end
+    local score = tonumber(l <= t) + tonumber(t <= r) + tonumber(l <= r) 
+    return (score % 2 == 1)
+end 
+
+function ArrDepTimetable:positionInInterval(t, l, r)
+    assert(t and l and r) 
+    self:assertInBounds({t, l, r})
+    if self:inInterval(t, l, r) then 
+        return {inside = true}
+    else 
+        local after = self:timeDiff(t, r) 
+        local before = self:timeDiff(l, t) 
+        return (after < before) and {after = after} or {before = before}
+    end 
+end 
 
 -- @return int? 
 function ArrDepTimetable:getDepartureTime(t)
     assert(t)
-    local res = {diff = self.period, value = nil}
+    local t = t % self.period 
+    local res = {score = nil, value = nil}
+    
     for _, arrdep in pairs(self.arrdeps) do
         local arr = arrdep.arr and arrdep.arr or arrdep.dep
-        local diff = self:absTimeDifference(arr, t % 3600)
-        if (diff < res.diff) then
-            res = {diff = diff, value = arrdep.dep}
+        local dep = arrdep.dep 
+        local pos = self:positionInInterval(t, arr, dep)
+        local k, v = next(pos)
+        if k == "inside" then 
+            return dep 
+        elseif (res.score == nil) or (v < res.score) then 
+            res.score = v
+            res.value = dep
         end
     end
 
--- tests/new_timetable_tests.lua
+++ tests/new_timetable_tests.lua
@@ -10,26 +10,27 @@ timetableTests[#timetableTests + 1] = function()
         {period = 3600, tests = {
             {0,0,0},
             {5,5,0},
-            {59,60,1},
+            {59,60,3599},
             {60,59,1},
             {10,5,5},
-            {0,5,5},
-            {3300,300,600},
+            {0,5,3595},
+            {3300,300,3000},
             {600,2400,1800}
         }}, 
         {period = 10, tests = {
-            {9, 0, 1},
+            {9, 0, 9},
             {0, 9, 1},
             {2, 8, 4},
             {2, 7, 5},
-            {2, 6, 4}
+            {2, 6, 6},
+            {6, 2, 4}
         }}
     }
     local i = 1
     for _,test_with_period in pairs(tests_with_periods) do
         local timetable = ArrDepTimetable:new(test_with_period.period)
         for _,test in pairs(test_with_period.tests) do 
-            local x = timetable:absTimeDifference(test[1], test[2])
+            local x = timetable:timeDiff(test[1], test[2])
             assert(x == test[3], 
                 string.format("test %d: Difference between %d and %d in period %d should be %d, got %d", 
                     i, test[1], test[2], test_with_period.period, test[3], x))
@@ -51,17 +52,18 @@ timetableTests[#timetableTests + 1] = function()
 
     local tests_with_periods = {
         {period = 60, tests = {
-            {{{30, 59}, {9, 58}}, 20, 59},
-            {{{30, 59}, {11, 58}}, 20, 58},
-            {{{51, 0}, {50, 59}}, 20, 0},
-            {{{51, 0}, {50, 59}}, 21, 59},
-            {{{1, 2}, {nil, 4}}, 2, 2},
-            {{{1, 2}, {nil, 4}}, 3, 4},
+            {{{30, 59}, {0, 9}}, 20, 59},
+            {{{30, 59}, {0, 11}}, 80, 11},
+            {{{30, 59}, {0, 11}}, 58, 59},
+            {{{30, 59}, {0, 11}}, 61, 11},
+            {{{58, 2}, {3, 4}}, 1, 2},
+            {{{1, 2}, {nil, 5}}, 63, 2},
+            {{{1, 2}, {nil, 5}}, 4, 5},
         }}, 
         {period = 13, tests = {
             {{}, 5, nil},
             {{{nil,2}}, 5, 2},
-            {{{nil,11}, {nil, 3}}, 0, 11},
+            {{{nil,11}, {nil, 3}}, 13, 11},
             {{{nil,11}, {nil, 3}, {nil, 1}}, 0, 1},
             {{{nil,0}, {nil, 3}, {nil, 1}}, 0, 0},
         }}
@@ -73,6 +75,7 @@ timetableTests[#timetableTests + 1] = function()
             local timetable = makeTimetable(test_with_period.period, test[1])
             local x = timetable:getDepartureTime(test[2])
             if test[3] then 
+                assert(x, string.format("test %d: The departure time should be %d, not nil", i, test[3])) 
                 assert(x == test[3], 
                     string.format("test %d: The departure time should be %d, got %d", i, test[3], x))
             else
